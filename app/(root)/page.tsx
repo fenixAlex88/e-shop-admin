026204f02f13e6b8f6a3c9e2e7f6fcd686c02d4f
Aleksey Pekar <fenix_al@tut.by>
2023-09-25 16:51:23 +0300

    db create store and change store

--- app/(root)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserButton } from "@clerk/nextjs";
-
-export default function SetupPage() {
-    return (
-        <div className="p-4">
-            <UserButton afterSignOutUrl='/'/>
-        </div>
-    );
-}
---
+++ components/ui/modal.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { DialogDescription, DialogContent, Dialog, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
+interface ModalProps {
+    title: string;
+    description: string;
+    isOpen: boolean;
+    onClose: () => void;
+    children?: React.ReactNode;
+}
+
+export const Modal: React.FC<ModalProps> = ({ title, description, isOpen, onClose, children }) => {
+    const onChange = (open: boolean) => {
+        if (!open) {
+            onClose();
+        }
+    };
+    return (
+        <Dialog open={isOpen} onOpenChange={onChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{description}</DialogDescription>
+                </DialogHeader>
+                <div>{children}</div>
+            </DialogContent>
+        </Dialog>
+    );
+};
